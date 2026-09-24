ee293e27301b39804898bcc6a32c860da78f3715
safamulla05-droid <safamulla05@gmail.com>
2026-09-24 23:48:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function addStudent() {
     alert("Add Student feature coming soon!");
 }
+
+function updateAttendance() {
+    const total = 1;
+    const present = 1;
+    const absent = total - present;
+
+    document.getElementById("totalStudents").textContent = total;
+    document.getElementById("presentStudents").textContent = present;
+    document.getElementById("absentStudents").textContent = absent;
+
+    const percentage = (present / total) * 100;
+    document.getElementById("attendancePercentage").textContent =
+        percentage.toFixed(0) + "%";
+}
+
+updateAttendance();
